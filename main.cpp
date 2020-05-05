4503d2c2709bd24064eb6fba9ca01d775ef1cd1c
Rotieh2000 <heiztorhssilva@gmail.com>
2020-05-05 10:07:07 -0300

    Leitura dos dados

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
-int main() {
-  std::cout << "Hello World!\n";
+using namespace std; 
+int main() 
+{
+
+string sexo;
+int cervejas, refri, espetos;
+
+cout << "Sexo: ";
+cin >> sexo;
+cout << "Quantidade de cervejas: ";
+cin >> cervejas;
+cout << "Quantidade de refrigerantes: ";
+cin >> refri;
+cout << "Quantidade de espetinhos: ";
+cin >> espetos;
+
+
+return 0; 
 }
